f3da11e42521834422295e2b3b51e67ba136838d
Francesco <pham.francesco@gmail.com>
2021-02-13 21:50:25 +0100

    use tensor2tensor pretrained evolved transformer

--- main.py
+++ main.py
@@ -1,38 +1,49 @@
-from model import *
-import json
+import tensorflow as tf
+from tensor2tensor import models
+from tensor2tensor import problems
+from tensor2tensor.utils import hparams_lib
+from tensor2tensor.utils import registry
+from tensor2tensor.data_generators import text_problems
+import numpy as np
 import re
-import tensorflow_datasets as tfds
 
-model_dir = "./italian_transformer_chatbot_v2/"
+MODEL_DIR = "./evolved_transformer_chatbot/"
+CHECKPOINT_NAME = "model.ckpt-50000"
+MODEL = "evolved_transformer"
+VOCAB_SIZE = 2**13
 
-with open(model_dir + 'hparams.json') as file:
-  hparams_json = json.load(file)
+tfe = tf.contrib.eager
+tfe.enable_eager_execution()
+Modes = tf.estimator.ModeKeys
 
-class HyperParameters:
-  def __init__(self, hparams_json) -> None:
-      self.num_heads = hparams_json["NUM_HEADS"]
-      self.d_model = hparams_json["D_MODEL"]
-      self.dropout = hparams_json["DROPOUT"]
-      self.activation = 'relu'
-      self.vocab_size = hparams_json["VOCAB_SIZE"]
-      self.num_encoder_layers = hparams_json["NUM_ENCODER_LAYERS"]
-      self.num_decoder_layers = hparams_json["NUM_DECODER_LAYERS"]
-      self.num_units = hparams_json["UNITS"]
-      self.start_token = hparams_json["START_TOKEN"]
-      self.end_token = hparams_json["END_TOKEN"]
-      self.max_length = hparams_json["MAX_LENGTH"]
+@registry.register_problem
+class ChatBot(text_problems.Text2TextProblem):
+    @property
+    def approx_vocab_size(self):
+        return VOCAB_SIZE
 
-hparams = HyperParameters(hparams_json)
 
-tokenizer = tfds.deprecated.text.SubwordTextEncoder.load_from_file(model_dir + "tokenizer")
-print("loaded tokenizer")
+chat_bot_problem = problems.problem("chat_bot")
+encoders = chat_bot_problem.feature_encoders(MODEL_DIR)
+hparams = hparams_lib.create_hparams_from_json(MODEL_DIR + 'hparams.json')
+hparams.data_dir = MODEL_DIR
+hparams_lib.add_problem_hparams(hparams, "chat_bot")
+ckpt_path = MODEL_DIR + CHECKPOINT_NAME
 
-model = transformer(hparams)
+chatbot_model = registry.model(MODEL)(hparams, Modes.PREDICT)
 
-model.load_weights(model_dir + 'checkpoint.h5')
-print("loaded model")
+def encode(input_str, output_str=None):
+    """Input str to features dict, ready for inference"""
+    inputs = encoders["inputs"].encode(input_str) + [1]  # add EOS id
+    batch_inputs = tf.reshape(inputs, [1, -1, 1])  # Make it 3D.
+    return {"inputs": batch_inputs}
 
-tf.random.set_seed(1234)
+def decode(integers):
+    """List of ints to str"""
+    integers = list(np.squeeze(integers))
+    if 1 in integers:
+        integers = integers[:integers.index(1)]
+    return encoders["inputs"].decode(np.squeeze(integers))
 
 def preprocess_sentence(sentence):
   sentence = sentence.lower().strip()
@@ -45,43 +56,16 @@ def preprocess_sentence(sentence):
   sentence = sentence.strip()
   return sentence
 
-def evaluate(sentence):
-  sentence = preprocess_sentence(sentence)
-
-  sentence = tf.expand_dims(
-      hparams.start_token + tokenizer.encode(sentence) + hparams.end_token, axis=0)
-
-  output = tf.expand_dims(hparams.start_token, 0)
-
-  for i in range(hparams.max_length):
-    predictions = model(inputs=[sentence, output], training=False)
-
-    # select the last word from the seq_len dimension
-    predictions = predictions[:, -1:, :]
-    predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
-
-    # return the result if the predicted_id is equal to the end token
-    if tf.equal(predicted_id, hparams.end_token[0]):
-      break
-
-    # concatenated the predicted_id to the output which is given to the decoder
-    # as its input.
-    output = tf.concat([output, predicted_id], axis=-1)
-
-  return tf.squeeze(output, axis=0)
-
-
-def predict(sentence):
-  prediction = evaluate(sentence)
-
-  predicted_sentence = tokenizer.decode(
-      [i for i in prediction if i < tokenizer.vocab_size])
-
-  return predicted_sentence
+def predict(inputs):
+    preprocessed = preprocess_sentence(inputs)
+    encoded_inputs = encode(preprocessed)
+    with tfe.restore_variables_on_create(ckpt_path):
+        model_output = chatbot_model.infer(encoded_inputs)["outputs"]
+    return decode(model_output)
 
 def main():
   while True:
-      sentence = input("You: ")
+      sentence = input("Input: ")
       print(predict(sentence))
 
 if __name__ == '__main__':
